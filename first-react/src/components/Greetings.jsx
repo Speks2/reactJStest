d43246636e2aa0b62b6b48c7ff46c68cdad88cb9
Speks2 <142408255+Speks2@users.noreply.github.com>
2024-08-12 10:39:17 +0200

    The beggining of ReactJS, fetched all the components

---
+++ first-react/src/components/Greetings.jsx
@@ -0,0 +1,16 @@
+// const Greeting = () => {
+//     return (
+//         <h1>Hello Idiota!</h1>
+//     )
+// }
+
+// function GreetingComponent() {
+//     return (
+
+//         <>
+//         <h1>Det her må man ikke!</h1>
+//         <h1>Hello you!</h1>
+//         </>
+        
+//     )
+// }
